59944282ee76329400b5baf18fc6767f76dc6a68
gbyers <geordie_2@email@geordienorman.com>
2014-12-31 02:55:41 +0000

    initial commit

---
+++ config.php
@@ -0,0 +1,39 @@
+<?php
+
+$admin_email="gbyers72@gmail.com";
+$admin_nick="gbyers";
+
+// nickname of your bot
+$irpg_bot="IdleRPG";
+
+// your game's server
+$irpg_network="irc.overdrive.pw";
+
+// your game's channel
+$irpg_chan="#IdleRPG";
+
+// full or relative pathname to the DBs:
+
+// character database
+$irpg_db="/home/geordie_2/idlerpg/irpg.db";
+
+// time modifiers file
+$irpg_mod="/home/geordie_2/idlerpg/modifiers.txt";
+
+// active quest info file
+$irpg_qfile="/home/geordie_2/idlerpg/questinfo.txt";
+
+// image to use for the top logo
+$irpg_logo="idlerpg.png";
+
+// directory in which your site is located from the root directory. my site
+// is http://jotun.ultrazone.org/g7/, so it's "/g7/"
+$BASEURL="/irpg/";
+
+// width-wise dimension of your map file
+$mapx = 500;
+
+// length-wise dimension of your map file
+$mapy = 500;
+
+?>
